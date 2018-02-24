cf9ae64fdeff8ff379a95dbee05d449e45c53c00
Benoit Rousselot <benoit.rousselot@raisepartner.com>
2018-02-24 12:03:08 +0100

    Add jsonBuilder

---
+++ buildJson.py
@@ -0,0 +1,28 @@
+import json
+import csv
+
+
+def BuildBasketComponents(sourceFile):
+	basketComponents = []
+	with open(sourceFile, 'rb') as csvfile:
+		spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+		for line in csvfile.readlines():
+			array = line.decode().split(',')
+			basketComponents.append({'instrumentId': array[1], 'amount': float(array[2])})
+	return basketComponents
+	
+basketId = 'myBasket'
+basketCurrency = 'USD'
+basketRiskFreeRate = 0.001
+estimationWindow = 120
+probabilityLevel = 'Percent97'
+horizon = 1
+
+data = {}  
+data['basket'] = { 'basketId': basketId, 'currency': basketCurrency, 'components': BuildBasketComponents('spx.csv'), 'riskFreeRate': basketRiskFreeRate }
+data['estimationWindow'] = estimationWindow
+data['probabilityLevel'] = probabilityLevel
+data['horizon'] = horizon
+		
+with open('jsonData.txt', 'w') as outfile:  
+    json.dump(data, outfile, indent=2)
